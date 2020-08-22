8ef6008723cb22636f900169b61bcf50b33d6e65
Jonatas Prates <jprates@dxc.com>
2020-08-22 16:53:55 -0300

    Simplificando a paginação e ordenação

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport //Para ativar a ordenação e paginação no spring http://localhost:8080/topicos/?page=0&size=10&sort=id,desc
 public class ForumApplication {
 
 	public static void main(String[] args) {
