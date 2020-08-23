229cb8c855ba5f01c8d12477c35af20c5b8a897c
Jonatas Prates <jprates@dxc.com>
2020-08-23 15:24:52 -0300

    Autenticando o cliente via Spring Security

--- src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -7,14 +7,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.alura.forum.model.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
 	
 	private TokenService tokenService;
+	private UsuarioRepository repository;
 	
-	public AutenticacaoViaTokenFilter(TokenService tokenService) {
+	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository repository) {
 		this.tokenService = tokenService;
+		this.repository = repository;
 	}
 
 	@Override
@@ -22,8 +29,19 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter{
 			throws ServletException, IOException {
 		String token = recuperarToken(request);
 		boolean valido = tokenService.isTokenValido(token);
+		
+		if (valido) {
+			autenticarCliente(token);
+		}
 
 		filterChain.doFilter(request, response);
+	}
+
+	private void autenticarCliente(String token) {
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = repository.findById(idUsuario).get();
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
 
 	private String recuperarToken(HttpServletRequest request) {
